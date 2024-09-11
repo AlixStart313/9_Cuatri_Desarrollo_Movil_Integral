ad28eeecc33aeb5d42ddb034d1a2f9099c1e6499
Lisseth Fuentes <105747977+AlixStart313@users.noreply.github.com>
2024-09-10 19:00:22 -0600

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,35 @@
+/**
+ * 
+ * 
+ */
+
+void main(){
+  String name="liss";
+  int edad= 21;
+  double altura=1.56;
+  bool isActive=true;
+  DateTime fechaNac=DateTime(2003,01,01);
+  String generos;
+  print("hola mundo $name");
+}
+
+class persona{
+  String name;
+  String Apellidos;
+  int edad;
+  double altura;
+  bool active;
+  DateTime cumple;
+
+  persona(
+    this.name,
+    this.Apellidos,
+    this.edad,
+    this.active,
+    this.altura,
+    this.cumple
+  
+  
+  );
+  
+}
