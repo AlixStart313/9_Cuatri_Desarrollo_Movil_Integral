a292e251962b11d84b3de19fdebd2d3f502f2b13
Lisseth Fuentes <105747977+AlixStart313@users.noreply.github.com>
2024-09-24 18:59:37 -0600

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\dev\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\CA3-SEP\Documents\Fuentes Figueroa Lisseth Georgina Web integral 10°\9_Cuatri_Desarrollo_Movil_Integral\flutter_application_1"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
